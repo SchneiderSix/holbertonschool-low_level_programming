2f07285fac6ee151caccf3270362f994c8a9e8ba
SchneiderSix <knuckles116635@gmail.com>
2022-06-06 07:10:34 -0700

    task four

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
-
+#print the thing in the puts with a new line and print zero
 
 #include <stdio.h>
 
-int main () {
+int main(void) {
 
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
