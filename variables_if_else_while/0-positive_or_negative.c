0efe2b478fb6056e04e68237e631204990d8b73d
SchneiderSix <knuckles116635@gmail.com>
2022-06-06 14:31:25 -0700

    task zero

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 /**
  * main - assign a random number to the variable n each
@@ -7,13 +8,22 @@
  */
 int main(void)
 {
-	int n random.Next(-10, 10);
+	int n;
+
+	strand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
+	}
 	else if (n < 0)
+	{
 		printf("%d is negative\n", n);
+	}
 	return (0);
 }
