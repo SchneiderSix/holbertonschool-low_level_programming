46873155c3b1c432b3aa4bc4bfa0e22db06ebecd
SchneiderSix <knuckles116635@gmail.com>
2022-06-06 07:22:08 -0700

    task four

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,12 @@
-/*print the thing in the puts with a new line and print zero*/
+/*
+ * print the thing in the puts with a new line and print zero
+*/
 
 #include <stdio.h>
 
 int main(void)
 {
-/* * holy ravioli
+/** holy ravioli
  * wow
  * nice
  */
