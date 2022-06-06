e2942b0753ffdc11f3d668aa6423be2f81e85ba4
SchneiderSix <knuckles116635@gmail.com>
2022-06-06 07:48:45 -0700

    task four

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,10 +7,10 @@
 #include <stdio.h>
 
 /**
- * Main - Prints "Programming is like building a multilingual puzzle, followed by a new line
+ * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
  *
- * Return - Always 0.
- *
+ * Return: Always 0.
+ *    
  */
 
 int main(void)
