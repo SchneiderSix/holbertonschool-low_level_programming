5927b72ff72b9422d0b7401ab46415b551b42dac
SchneiderSix <knuckles116635@gmail.com>
2022-06-06 07:45:17 -0700

    task four

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,4 @@
-/**
+/*
  * main - Rip god
  * puts - print things
  * return - print zero
@@ -7,8 +7,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
- * return - Always 0.
+ * Main - Prints "Programming is like building a multilingual puzzle, followed by a new line
+ *
+ * Return - Always 0.
+ *
  */
 
 int main(void)
