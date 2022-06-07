f3bf6d7aa1ebe262f9a55b3e743193604dbd1faf
SchneiderSix <knuckles116635@gmail.com>
2022-06-07 07:51:09 -0700

    task ten

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of
+ * two digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int first;
+	int second;
+
+	for (first = 48; first < 58; first++)
+	{
+		for (second = 48; second < 58; second++)
+		{
+			if (first + second != second + first)
+			{
+				putchar(first);
+				putchar(second);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
+}
