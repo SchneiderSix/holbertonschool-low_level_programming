d0b8cfaab828c6e6942186ea5dc5db720fbfda01
SchneiderSix <knuckles116635@gmail.com>
2022-06-08 09:29:27 -0700

    task zero

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - print things
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n = '_putchar';
+	print(n);
+	putchar('n');
+	return (0);
+}
