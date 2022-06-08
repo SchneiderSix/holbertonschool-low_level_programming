f488b32426a311adcda434894abf81410f0a66b5
SchneiderSix <knuckles116635@gmail.com>
2022-06-08 16:09:21 -0700

    task three

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - prints the alphabet
+ */
+
+int _islower(int c)
+{
+	int letter;
+
+	letter = 'c';
+
+	if(isalpha(letter) != 0) _putchar('1');
+	else _putchar('0');
+	
+	_putchar('\n');
+}
