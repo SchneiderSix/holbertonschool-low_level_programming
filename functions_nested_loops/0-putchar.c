61707809a82ddfc460552c0ec0845aa4a6fb1d5e
SchneiderSix <knuckles116635@gmail.com>
2022-06-08 09:40:04 -0700

    task zero

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,8 +7,14 @@
 
 int main(void)
 {
-	char n = '_putchar';
-	print(n);
-	putchar('n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
