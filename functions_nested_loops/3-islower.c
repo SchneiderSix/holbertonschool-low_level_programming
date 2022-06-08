cb31867d2d53754cfd64a763723c76c00956b38f
SchneiderSix <knuckles116635@gmail.com>
2022-06-08 16:28:51 -0700

    task three

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -8,10 +8,14 @@ int _islower(void)
 {
 	int letter;
 
-	letter = 'c';
-
-	if (islower(letter)) _putchar('1');
-	else _putchar('0');
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 	
 	_putchar('\n');
 }
