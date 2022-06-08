6ce97f75f0e6e7eac5d6d493878f062fc3fec036
SchneiderSix <knuckles116635@gmail.com>
2022-06-08 14:13:57 -0700

    task two

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ * 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char count;
+	char x;
+
+	while (count = '0'; count < '10')
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		count++
+	}
+	_putchar('\n');
+}
