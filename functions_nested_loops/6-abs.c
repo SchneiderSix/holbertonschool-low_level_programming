fafaf376b4663afbe6508b4b71fff245ce20c207
SchneiderSix <knuckles116635@gmail.com>
2022-06-08 17:32:24 -0700

    task six

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_sign - print numb
+ * @n: int var cheked
+ * Return: 1 for positive, -1 for negative
+ * 0 for 0
+ */
+
+int _abs(int)
+{
+	int n;
+	return n*(2*n+1)%2);
+}
