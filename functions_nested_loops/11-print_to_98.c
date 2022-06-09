08aca335ced42f019c3d040e5ade22a558946b4c
SchneiderSix <knuckles116635@gmail.com>
2022-06-09 06:29:23 -0700

    task eleven

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print nat num
+ * until 98
+ * @n: var int
+ * Return:  return result
+ */
+
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar(n);
+		_putchar('\n');
+}
