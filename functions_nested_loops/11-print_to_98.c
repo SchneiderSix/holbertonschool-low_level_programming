211461ddcbae2a87d91a47c51438f7b629b702d2
SchneiderSix <knuckles116635@gmail.com>
2022-06-09 10:58:02 -0700

    task eleven

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	while (n <= 98)
 	{
-		if (n = 98)
+		if (n == 98)
 		{
 			printf("98");
 			printf("\n");
@@ -21,7 +21,7 @@ void print_to_98(int n)
 		else
 		{
 		printf("%d, ", n);
-		n++
+		n++;
 		}
 	}
 }
