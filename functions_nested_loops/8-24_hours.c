4a399aa89e82865687449ce9b362a9b6470b0310
SchneiderSix <knuckles116635@gmail.com>
2022-06-08 18:26:27 -0700

    task eight

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,15 +10,18 @@ void jack_bauer(void)
 
 	int h, m;
 
-	for (h = 0; h <= 23; h++)
+	for (h = 0; h < 24)
 	{
-		for (m = 0; m <= 59; m++)
+		for (m = 0; m < 60)
 		{
 			_putchar ((h / 10) + '0');
 			_putchar ((h % 10) + '0');
 			_putchar (':');
 			_putchar ((m / 10) + '0');
 			_putchar ((m % 10) + '0');
+			_putchar ('\n');
+			m++;
 		}
+		h++;
 	}
 }
