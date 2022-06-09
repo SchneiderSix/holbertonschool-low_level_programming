3b39622a8896edd66d2894de2abc995a2cdcbcee
SchneiderSix <knuckles116635@gmail.com>
2022-06-08 20:46:32 -0700

    task nine

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - prints every minute
+ * Return: last digit
+ */
+
+void times_table(void)
+{
+
+	int columnzeros, rowscont, mult;
+
+	for (columnzeros = '0'; columnzeros <= '9'; columnzeros++)
+	{
+		_putchar('0');
+
+		for (rowscont = '1'; rowscont <= '9'; rowscont++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			
+			mult = columnzeros * rowscont;
+
+			if (mult <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((mult / 10) + '0';
+			}
+			_putchar((mult % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
