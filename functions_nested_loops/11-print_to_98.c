7af7464f8fdfd17c058ddf855b4901fd49134b20
SchneiderSix <knuckles116635@gmail.com>
2022-06-09 11:31:03 -0700

    task eleven

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,23 +10,18 @@
 
 void print_to_98(int n)
 {
-	while (n <= 111 || n <= 0)
+	while (n != 98)
 	{
-		if (n == 98)
-		{
-			printf("98");
-			printf("\n");
-			break;
-		}
-		else if ( n <= 0)
+		if ( n > 98)
 		{
 			printf("%d, ", n);
-			n++;
-		}
-		else if ( n > 98)
-		{
-			printf("%d, ",n);
 			n--;
 		}
+		else if ( n < 98)
+		{
+			printf("%d, ",n);
+			n++;
+		}
 	}
+	printf("%d\n", n);
 }
