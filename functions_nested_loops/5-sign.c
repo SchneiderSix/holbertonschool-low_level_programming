1f5b75e59ab0d0f315b17967d231608d64791b0f
SchneiderSix <knuckles116635@gmail.com>
2022-06-08 17:26:12 -0700

    task five

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isalpha - check the char
+ * print_sign - print numb
  * @n: int var cheked
- * Return: 1 for alphabetic char
- * 0 for otherwise
+ * Return: 1 for positive, -1 for negative
+ * 0 for 0
  */
 
 int print_sign(int n)
 {
-	if (n > 0 )
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
