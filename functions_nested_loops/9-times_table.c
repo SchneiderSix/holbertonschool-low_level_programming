87617462a9c489be82484f79ba1c9207941637a5
SchneiderSix <knuckles116635@gmail.com>
2022-06-08 20:51:25 -0700

    task nine

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -24,12 +24,13 @@ void times_table(void)
 			if (mult <= 9)
 			{
 				_putchar(' ');
+				_putchar(mult);
 			}
 			else
 			{
 				_putchar((mult / 10) + '0');
+				_putchar((mult % 10) + '0');
 			}
-			_putchar((mult % 10) + '0');
 		}
 		_putchar('\n');
 	}
