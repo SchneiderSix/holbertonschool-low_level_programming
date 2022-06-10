852ef3fde7a8bcfcbc0fd8778ce754d2d64b78f6
SchneiderSix <knuckles116635@gmail.com>
2022-06-09 18:05:09 -0700

    task zero

---
+++ debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - testing func if int is
+ * positv or neg
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
