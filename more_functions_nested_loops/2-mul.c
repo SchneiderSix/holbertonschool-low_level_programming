4ba5ad6a39447ba6e3fe0b5b7eee700177d25a13
SchneiderSix <knuckles116635@gmail.com>
2022-06-10 07:38:22 -0700

    task two

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two int
+ * @a: int var
+ * @b: int var
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	_putchar(c);
+	return (0);
+}
