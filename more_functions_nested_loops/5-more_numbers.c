aef7b38fa157d5f079dd58b3337f24d62a0ea2e4
SchneiderSix <knuckles116635@gmail.com>
2022-06-10 10:33:25 -0700

    task five

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,16 @@ void more_numbers(void)
 {
 	int count, n;
 
-	for (count = 1; count <= 10; count++)
+	for (count = 1; count < 10; count++)
 	{
 
-		for (n = 0; n <= 14; n++)
+		for (n = 0; n < 15; n++)
 		{
 			if (n < 10)
 			{
 				_putchar((n / 10) + '0');
 			}
-			else
-			{
-				_putchar((n % 10) + '0');
-			}
+			_putchar((n % 10) + '0')
 		count++;
 		}
 	}
