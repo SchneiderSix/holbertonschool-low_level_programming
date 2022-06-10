5e365941cbeb6940cef786a351730f6ab6428038
SchneiderSix <knuckles116635@gmail.com>
2022-06-10 15:20:36 -0700

    task seven

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print numbers
+ * @n: int var
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	while (n > 0)
+	{
+		_putchar(' ');
+		n--;
+	}
+	_putchar('\');
+	_putchar('\n');
+}
