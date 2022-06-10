79bd13f14360f1936ff0327256ec83ce7fbd18d0
SchneiderSix <knuckles116635@gmail.com>
2022-06-10 06:36:47 -0700

    task zero

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _isupper - uppercase test
+ * @c: char var
+ * Return: 1 if upp, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A'; && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
