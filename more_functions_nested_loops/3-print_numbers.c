0a588ee7532d83c855f2e983f5ca1491c80aa38c
SchneiderSix <knuckles116635@gmail.com>
2022-06-10 09:04:26 -0700

    task three

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * Return: Always 0.
+ */
+
+void print_numbers(void);
+{
+	int pepito;
+
+	for (pepito = 0; pepito < 10; pepito++)
+	{
+		_putchar(pepito);
+	}
+	_putchar(' ');
+	return (0);
+}
