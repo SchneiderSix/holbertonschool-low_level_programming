dc2047f96895ec155196841de098eb229e955396
SchneiderSix <knuckles116635@gmail.com>
2022-06-10 10:02:26 -0700

    task five

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int pepito;
+	int count;
+
+	count = 1;
+
+	while (count < 11)
+	{
+
+		for (pepito = "0"; pepito <= "14"; pepito++)
+		{
+			if (pepito < "10")
+			{
+				_putchar((pepito / 10) + '0');
+			}
+			else
+			{
+				_putchar((pepito % 10) + '0');
+			}
+		count++
+		}
+	}
+	_putchar('\n');
+}
