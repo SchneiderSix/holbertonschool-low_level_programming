e64ac658059ab1647398d2592356dc9825d33dba
SchneiderSix <knuckles116635@gmail.com>
2022-06-10 09:19:29 -0700

    task four

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int pepito;
+
+	for (pepito = 48; pepito <= 57; pepito++)
+	{
+		if ((pepito != 2) || (pepito != 4))
+		{
+			_putchar(pepito);
+		}
+	}
+	_putchar('\n');
+}
