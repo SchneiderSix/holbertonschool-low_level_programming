ac33819b499f349b0d4b8c4e739f7b7ee5b6042b
SchneiderSix <knuckles116635@gmail.com>
2022-06-10 18:45:15 -0700

    task eight

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_square - print numbers
+ * @size: int var
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int hiq, liq;
+
+		for (hiq = 0; hiq < size; hiq++)
+		{
+			for (liq = 0; liq < size; liq++)
+			{
+				if (hiq == liq)
+				{
+					_putchar('#');
+					_putchar('\n');
+				}
+				else if (liq < hiq)
+				{
+					_putchar('#');
+				}
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
