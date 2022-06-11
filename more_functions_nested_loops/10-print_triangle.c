3167b5f4c8d2527bd45c6f8538000c6e1f472405
SchneiderSix <knuckles116635@gmail.com>
2022-06-10 19:59:38 -0700

    task ten

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - print numbers
+ * @size: int var
+ * Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i, n;
+		
+		for (i = 1; i > 0; i--)
+		{
+			for (n = size; n < i; j++)
+			{
+				_putchar(#);
+			}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
