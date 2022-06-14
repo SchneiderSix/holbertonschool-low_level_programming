6928f7b0aa8109807721d6b9248e29e57f076f21
SchneiderSix <knuckles116635@gmail.com>
2022-06-14 14:22:02 -0700

    task five

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: var
+ *Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int last;
+	int pepito;
+	char [pepito];
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	last = i - 1;
+
+	for (pepito = 0; pepito < i/2; pepito++)
+	{
+		temp = s[pepito];
+		s[pepito] = s[last];
+		s[pepito--] = temp;
+	}
+}
