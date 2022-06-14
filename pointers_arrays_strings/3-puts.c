518634e5991cfe4d01b0b0d308b1eacb4cdd5dba
SchneiderSix <knuckles116635@gmail.com>
2022-06-14 10:32:44 -0700

    task three

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - Write a function that
+ * prints a string followed by
+ * a new line to stdout
+ * @str: var
+ *Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	int count;
+
+	for (count = 0; str[i] != '\0''; count++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
