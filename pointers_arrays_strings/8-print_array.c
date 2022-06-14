3340670fd0c0bd5dd6dbab0abb8b5671b0092542
SchneiderSix <knuckles116635@gmail.com>
2022-06-14 16:50:20 -0700

    task eight

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void print_array - function that prints
+ * n elements of an array of integers
+ * , followed by a new line
+ * @n: var
+ * @*a: pointer
+ *Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int count;
+
+	for (count = 0; count < n; count++)
+	{
+		if (a[count] != '\0')
+		{
+			printf("%p, ", a[count];
+		}
+		else
+		{
+			printf("%p", a[count];
+		}
+	}
+}
