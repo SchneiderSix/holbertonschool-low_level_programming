5cd5fb3c5eedd5d16583ea941c2eb9dc5f49746a
SchneiderSix <knuckles116635@gmail.com>
2022-06-15 10:47:55 -0700

    task two 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy a string
+ * @dest: var
+ * @src: vari
+ * @n: var
+ *Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j, k;
+
+	j = 0;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[j] = src[j]
+		j++;
+	}
+	dest[j] = '\0';
+
+	return (dest);
+}
