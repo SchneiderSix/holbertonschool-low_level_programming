613f9b96c45fb510dc8f0a6ca24e1d0d9b440fd7
SchneiderSix <knuckles116635@gmail.com>
2022-06-16 13:45:04 -0700

    task six 2

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *cap_string - explicit name
+ * capitalizes all words of a string
+ * @h: var
+ *Return: Always 0.
+ */
+
+char *cap_string(char *h)
+{
+	int i;
+
+	i = 0;
+
+	while (h[i] != '\0')
+	{
+		i++;
+		while (!(h[i] >= 'a' && h[i] <= 'z'))
+		{
+			i++
+		}
+		if (h[i - 1] == '0' || h[i - 1] == ' ' || h[i - 1] == '\t' ||
+		h[i - 1] == '\n' || h[i - 1] == ',' ||
+		h[i - 1] == ';' || h[i - 1] == '.' ||
+		h[i -1 ] == '!' || h[i - 1] == '?' ||
+		h[i - 1] == '"' || h[i - 1] == '(' ||
+		h[i - 1] == ')' || h[i - 1] == '{' ||
+		h[i - 1] == '}')
+		{
+			h[i] -= 32;
+		}
+	}
+	return (h);
+}
