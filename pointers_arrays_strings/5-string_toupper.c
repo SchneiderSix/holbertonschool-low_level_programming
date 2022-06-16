87bc6e73cd46cd19a62f34c4f5b706910dd0d37e
SchneiderSix <knuckles116635@gmail.com>
2022-06-16 08:30:27 -0700

    task five 2

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *string_toupper - explicit name
+ * @*: var
+ *Return: Always 0.
+ */
+
+char *string_toupper(char *)
+{
+	const char comp = 'a' - 'A';
+
+	while (*)
+	{
+		* = (* >= 'a' && * <= 'A') ? * -= comp : *;
+	}
+}
