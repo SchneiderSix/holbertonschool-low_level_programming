969a3644837e16e2e19d7480c97dc12e7eb53f83
SchneiderSix <knuckles116635@gmail.com>
2022-06-16 07:57:27 -0700

    task three 2

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - copy a string
+ * @s1: var
+ * @s2: vari
+ *Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	size_t n;
+
+	while ( n && *s1 && (*s1 == *s2))
+	{
+		++s1;
+		++s2;
+		--n;
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return ( *(unsigned char *)s1 - *(unsigned char *)s2 );
+	}
+}
