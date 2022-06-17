6ca027a83193d4f57d9378b0e65f9e1b95f2e075
SchneiderSix <knuckles116635@gmail.com>
2022-06-16 17:52:02 -0700

    task zero 3

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with
+ * constant byte
+ * @s: var
+ * @b: var
+ * @n: var
+ *Return: Always 0.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned char *d = s;
+
+	while (n > 0)
+	{
+		*d = char b;
+		d++;
+		n--;
+	}
+
+	return (s);
+}
