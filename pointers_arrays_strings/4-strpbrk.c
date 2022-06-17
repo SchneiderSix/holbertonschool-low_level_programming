4135709faf2e4d8e34613c3dd2e77bebbcf7929a
SchneiderSix <knuckles116635@gmail.com>
2022-06-17 05:14:18 -0700

    task four 3

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strpbrk - return pointer
+ * to the  first
+ * occurrence in string s of any
+ * of th bytes in string accept
+ * @s: var
+ * @accept: var
+ * Return: return numb of bytes
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	size_t i, p;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (p = 0; accept[p] != s[i]; p++)
+		{
+			if (accept[p] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+
+	return ('\0');
+}
