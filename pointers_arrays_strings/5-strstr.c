117158ccb39bbf89009f42cb8246d39907c71040
SchneiderSix <knuckles116635@gmail.com>
2022-06-17 05:41:10 -0700

    task five 3

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *_strstr - function finds the first occurrence of the substring
+ * needle in the string haystack.
+ * The terminating null bytes (\0) are not compared
+ * Returns a pointer to the beginning of the located
+ * substring, or NULL if the substring is not found.
+ * to the  first
+ * occurrence in string s of any
+ * of th bytes in string accept
+ * @needle: var
+ * @haystack: var
+ * Return: return numb of bytes
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*haystack)
+	{
+		for (i = 0; needle[i] != '\0'; i++)
+		{
+			if (*haystack == needle[i])
+			{
+				return (s);
+			}
+		}
+		haystack++;
+	}
+
+	return ('\0');
+}
