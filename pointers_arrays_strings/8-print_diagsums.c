5bc8947b9f3b1296f5acbc2232c790a1d873805a
SchneiderSix <knuckles116635@gmail.com>
2022-06-17 07:27:16 -0700

    task seven 3

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_diagsums - sum in diagonal
+ * @a: var
+ * Return: Always 0.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int fd;
+	int sd;
+	int i;
+	int k;
+
+	fd = 0;
+
+	sd = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (k = 0; k < size; k++)
+		{
+			if (i == k)
+			{
+				fd += a[i][k];
+			}
+			if ((i + k) == (size - 1))
+			{
+				sd += a[i][k];
+			}
+		}
+	}
+		_putchar(fd);
+		_putchar(", "sd);
+
+}
