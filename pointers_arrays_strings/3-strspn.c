da7581bf0693ec022bde31dc6c0fc44e9d548f95
SchneiderSix <knuckles116635@gmail.com>
2022-06-17 05:04:34 -0700

    task three 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strspn - return numb of bytes
+ * in a string
+ * @s: var
+ * @accept: var
+ * Return: return numb of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	size_t i, p;
+
+	for (i = 0; s[i] !='\0'; i++)
+	{
+		for (p = 0; accept[p] != s[i]; p++)
+		{
+			if (accept[p] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+
+	return (i);
+}
