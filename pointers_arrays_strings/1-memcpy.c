6b6e0dc596214cea84407ec75d9b671ebaa7b6db
SchneiderSix <knuckles116635@gmail.com>
2022-06-16 18:23:05 -0700

    task one 3

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memcpy - cp bytes from memory
+ * area to another mem area
+ * @dest: var
+ * @src: var
+ * @n: var
+ *Return: Always 0.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
