7fb84766df8c02045bb244559de7aff1e100ccc3
SchneiderSix <knuckles116635@gmail.com>
2022-06-20 08:23:01 -0700

    task two

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _strlen_recursion - print str lenght
  * @s: string
+ * Return: len or 0
  */
 
 int _strlen_recursion(char *s)
@@ -13,6 +14,6 @@ int _strlen_recursion(char *s)
 	}
 	else
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
