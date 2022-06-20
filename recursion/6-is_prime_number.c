fe05b25a948a4e2bca96081221d46dd80c5b0d4d
SchneiderSix <knuckles116635@gmail.com>
2022-06-20 12:19:23 -0700

    task six

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prime numbers
+ * @n: num
+ * Return: 1 if n is prime, otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	int k;
+
+	k = n;
+
+	if (n % 1 == 0 && n % k == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
