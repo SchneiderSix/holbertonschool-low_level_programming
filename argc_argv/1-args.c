058932a5e961429b3d28e0cb5f2a2986c87427e1
SchneiderSix <knuckles116635@gmail.com>
2022-06-21 11:11:40 -0700

    task one

---
+++ argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print name of program with
+ * new line
+ * @argc: numb of comm line arguments
+ * @argv: array of argc
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = =; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
