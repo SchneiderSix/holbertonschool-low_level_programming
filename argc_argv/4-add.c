06facc96ad62e88c6c82c180157131688872b323
SchneiderSix <knuckles116635@gmail.com>
2022-06-21 13:30:55 -0700

    task four

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
 		result = result + x;
 
-		if (x > '9' || x < '0')
+		if (((x / 10) + '0') > '9' || ((x / 10) + '0') < '0')
 		{
 			printf("Error\n");
 			return (1);
