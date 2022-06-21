89b4c2b9a744910893c5467731794edb1a393b3a
SchneiderSix <knuckles116635@gmail.com>
2022-06-21 13:14:25 -0700

    task four

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - print name of program with
@@ -24,7 +25,7 @@ int main(int argc, char *argv[])
 
 		result = result + x;
 
-		if (argv[2] == NULL)
+		if (!isdigit(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
