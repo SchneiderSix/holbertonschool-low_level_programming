50ce2a5026473a885102f3da529ace1d911dce77
SchneiderSix <knuckles116635@gmail.com>
2022-06-21 13:08:29 -0700

    task four

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print name of program with
+ * new line
+ * @argc: numb of comm line arguments
+ * @argv: array of argc
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	int result;
+
+	result = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		int x = strtol(argv[i], NULL, 10);
+
+		result = result + x;
+
+		if (argv[2] == NULL)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
