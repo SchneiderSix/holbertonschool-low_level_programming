422bfecfd4ead7043f6348630877e459c9bfbbe6
SchneiderSix <knuckles116635@gmail.com>
2022-06-22 12:44:09 -0700

    task zero

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,12 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * create_array - crate array of chars
+ * starting from c
+ * @size: size of the array
+ * @c: starting char
+ */
+
 char *create_array(unsigned int size, char c)
 {
 	int i;
-	char *p;
-
-	*p =  malloc(sizeof(char) * size);
+	char *p =  malloc(sizeof(char) * size);
 
 	if (p == NULL || size == 0)
 	{
