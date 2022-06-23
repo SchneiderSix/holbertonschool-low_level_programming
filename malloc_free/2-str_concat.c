ef6ed2f9fdde284331c76b95963ae3f6e2d24020
SchneiderSix <knuckles116635@gmail.com>
2022-06-22 20:50:59 -0700

    task two

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,6 +16,11 @@ char *str_concat(char *s1, char *s2)
 
 	char *x = malloc(strlen(s1) + strlen(s2) + 1);
 
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL)
+	}
+
 	for (i = 0; (x[k] = s1[i]); ++i, ++k)
 	{
 	}
