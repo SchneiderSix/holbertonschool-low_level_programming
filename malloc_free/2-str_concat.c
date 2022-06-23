c24bdc353c1e6403bf9a47834a58c108c6caa72a
SchneiderSix <knuckles116635@gmail.com>
2022-06-22 20:31:44 -0700

    task two

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,14 +16,13 @@ char *str_concat(char *s1, char *s2)
 
 	char *x = malloc(strlen(s1) + strlen(s2) + 1);
 
-	for (i = 0; (x[k] = s1[i]); ++i, ++k)
+	for (i = 0; (x[k] = s1[i]) != '\0'; ++i, ++k)
 	{
 	}
-	for (i = 0; (x[k] = s2[i]); ++i, ++k)
+	for (i = 0; (x[k] = s2[i]) != '\0'; ++i, ++k)
 	{
 	}
 	printf("%s\n", x);
-	free(x);
 	return (0);
 
 }
