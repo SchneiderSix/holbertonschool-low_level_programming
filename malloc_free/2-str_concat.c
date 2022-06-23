172bba4ddb2ee5536c8a2a79337e58e0c035acb2
SchneiderSix <knuckles116635@gmail.com>
2022-06-22 21:42:03 -0700

    task two

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -21,24 +21,20 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
+	if (s1 == NULL)
+	{
+		*s1 == "";
+	}
+	if (s2 == NULL)
+	{
+		*s2 == "";
+	}
 
 	for (i = 0; (x[k] = s1[i]); ++i, ++k)
 	{
-		if (s1 == NULL)
-		{
-			s1 = "";
-		}
 	}
 	for (i = 0; (x[k] = s2[i]); ++i, ++k)
 	{
-		if (s2 == NULL)
-		{
-			s2 = "";
-		}
-	}
-	if (s1 == NULL && s2 == NULL)
-	{
-		return ("");
 	}
 
 	return (x);
