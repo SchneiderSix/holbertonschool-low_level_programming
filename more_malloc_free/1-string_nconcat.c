82d4dece63c2f1913c3691c772433d6a59da774d
SchneiderSix <knuckles116635@gmail.com>
2022-06-24 10:55:44 -0700

    task one

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		m = malloc(sizeof(char) * (len1 + len2 + 2:));
+		m = malloc(sizeof(char) * (len1 + len2 + 2));
 	}
 	if (m == NULL)
 	{
