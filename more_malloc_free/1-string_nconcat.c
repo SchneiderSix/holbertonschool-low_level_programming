cdf7da778c38138eec855996bd3882391e305daa
SchneiderSix <knuckles116635@gmail.com>
2022-06-24 08:46:06 -0700

    task one

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j, len;
+	unsigned int i, j, len;
 	char *m;
 
 	if (s1 == NULL)
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			j = n;
 		}
 	}
-	len = j + i
+	len = j + i;
 	m = malloc(sizeof(char) * (len + 1));
 
 	if (m == NULL)
