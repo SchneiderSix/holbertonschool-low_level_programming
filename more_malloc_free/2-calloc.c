848ac737b68cc8949dd28a5a30ec42e9807aa1a3
SchneiderSix <knuckles116635@gmail.com>
2022-06-24 11:09:58 -0700

    task two

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_calloc - alloc mem for arr
+ * @nmemb: elements
+ * @size: bytes
+ * Return: pointer or null
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *m;
+	int i;
+
+	for (i = 0; i < nmemb; i++)
+	{
+		m[i] = calloc(sizeof(char);
+	}
+	return (m);
+}
