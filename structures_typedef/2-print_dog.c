cdc3e255f6b78b998eac37f6bba269231b49c274
SchneiderSix <knuckles116635@gmail.com>
2022-06-27 18:26:22 -0700

    task two

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * init_dog - initialize a var of type
+ * @d: pointer to struct dog initialize
+ * @name: var
+ * @age: var
+ * @owner: var
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if (d->name = NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->owner = NULL)
+	{
+		d->owner = "(nil)";
+	}
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
