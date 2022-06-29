ba6bfec1de888df3f0c19c4600302ba691fd6d3d
SchneiderSix <knuckles116635@gmail.com>
2022-06-28 18:24:00 -0700

    task three

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum
+ * @a: var
+ * @b: var
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *  * op_sub - difference
+ *   * @a: var
+ *    * @b: var
+ *     * Return: result
+ *      */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *  * op_mul - multiplication
+ *   * @a: var
+ *    * @b: var
+ *     * Return: result
+ *      */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *  * op_div - div
+ *   * @a: var
+ *    * @b: var
+ *     * Return: result
+ *      */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *  * op_mod - resto de division
+ *   * @a: var
+ *    * @b: var
+ *     * Return: result
+ *      */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
