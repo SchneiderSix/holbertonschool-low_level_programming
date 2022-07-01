7c017a8fc840cbe94044fabbb1b2e0a7e05d8052
SchneiderSix <knuckles116635@gmail.com>
2022-07-01 08:24:32 -0700

    task one

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(list, int));
 
-		while (i < (n - 1))
+		if (i < (n - 1))
 		{
 			printf("%s", separator);
 		}
