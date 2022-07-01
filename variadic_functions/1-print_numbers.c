dca203bb0ebb5d25c761e126cfbb0eff315e41ba
SchneiderSix <knuckles116635@gmail.com>
2022-07-01 08:42:30 -0700

    task one

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -28,6 +28,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		}
 
 	}
-	printf("%d\n", va_arg(list, int));
+	printf("%d", va_arg(list, int));
+	printf("\n");
 	va_end(list);
 }
