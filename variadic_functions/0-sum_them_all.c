676656afa6ceaa6554de173897ee7bd34d3f6f13
SchneiderSix <knuckles116635@gmail.com>
2022-07-01 07:50:24 -0700

    task zero

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - sum of all its parameters
+ * @n: var positive
+ * Return: 0 or sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	int i, sum;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start (list, n);
+
+	sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg (list, int);
+	}
+	va_end (list);
+	return (sum);
+}
