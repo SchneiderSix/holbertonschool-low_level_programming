4b80894893c147e6ec38f8eb277368cf2f08dafa
SchneiderSix <knuckles116635@gmail.com>
2022-07-12 08:05:11 -0700

    task zero

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - singly linked list
+ * @h: pointer to list_t
+ * Return: count of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%lu] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
