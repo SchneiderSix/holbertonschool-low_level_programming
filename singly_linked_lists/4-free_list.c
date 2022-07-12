4a0554816d3ede01797caa2d495d7f72fe7fab7f
SchneiderSix <knuckles116635@gmail.com>
2022-07-12 11:19:46 -0700

    task four

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - singly linked list
+ * @head: list_t to be free
+ * Return: new address of element or null
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmpnode;
+
+	while (head)
+	{
+		tmpnode = head;
+		head = head->next;
+		free(tmpnode);
+	}
+}
