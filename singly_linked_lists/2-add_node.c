0395e30b46fb14d77c881a279f029a7ef675d030
SchneiderSix <knuckles116635@gmail.com>
2022-07-12 10:02:27 -0700

    task two

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * add_node - singly linked list
+ * @h: pointer to list_t
+ * Return: count of nodes
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnonode;
+	int len = 0;
+
+	newnode = malloc(sizeof(list_t));
+
+	if (!newnode)
+	{
+		return (NULL);
+	}
+
+	while (str[len])
+	{
+		len++;
+	}
+	newnode->len = len;
+	newnode->str = strup(str);
+	newnode->next = (*head);
+	(*head) = newnode;
+
+	return (*head);
+}
