1979d0e932eea30180a32109df0628d431de4656
SchneiderSix <knuckles116635@gmail.com>
2022-07-12 09:29:19 -0700

    task one

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_list - singly linked list
+ * @h: pointer to list_t
+ * Return: count of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
