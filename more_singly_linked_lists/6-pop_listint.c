7909d061871cfb37787c29fd2ac0ac71f7cd0f92
SchneiderSix <knuckles116635@gmail.com>
2022-07-13 06:06:23 -0700

    task six

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * pop_listint - singly linked list
+ * @head: pointer of listint_t
+ * Return: count of nodes
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int n;
+
+	if (!head || !*head)
+	{
+		return (0);
+	}
+
+	while (*head)
+	{
+		n = (*head)->n;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
+	return (n);
+}
