aa29b8d62e62b30b382893cf124dd32f70c98afa
SchneiderSix <knuckles116635@gmail.com>
2022-07-13 08:55:48 -0700

    task ten

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	if (index == 0)
 	{
-		head = head->next;
+		*head = *head->next;
 		tmp->next = NULL;
 		free(tmp);
 		return (1);
