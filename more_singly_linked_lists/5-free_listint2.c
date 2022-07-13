357f878def78b5c8d25e49a6099d9ce7551e1f6d
SchneiderSix <knuckles116635@gmail.com>
2022-07-13 05:39:58 -0700

    task five

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - singly linked list
+ * @head: pointer of listint_t
+ * Return: count of nodes
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+	}
+}
