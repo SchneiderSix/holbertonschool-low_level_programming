510e1861f1037f48c462ee2bff0ae3660fe7231e
SchneiderSix <knuckles116635@gmail.com>
2022-07-13 05:23:38 -0700

    task three

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * add_nodeint - singly linked list
+ * add_nodeint_end - singly linked list
  * @head: double pointer of listint_t
  * @n: int var
  * Return: count of nodes
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *nn, *ln = *head;
 
