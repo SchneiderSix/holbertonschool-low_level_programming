4816f9f0bf240ecac50801c63684c508faa70f2e
SchneiderSix <knuckles116635@gmail.com>
2022-07-13 08:17:18 -0700

    task nine

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *nn, *tmp = *head;
-	unsigned int;
+	unsigned int i;
 
 	nn = malloc(sizeof(listint_t));
 
