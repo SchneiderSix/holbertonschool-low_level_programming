296d18c9dfebfd45446243c15cc0243fd50a4820
SchneiderSix <knuckles116635@gmail.com>
2022-07-13 05:44:39 -0700

    task five

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,9 @@ void free_listint2(listint_t **head)
 
 	while (head)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		tmp = *head->next;
+		free(*head);
+		*head = tmp;
 	}
 	*head = NULL;
 }
