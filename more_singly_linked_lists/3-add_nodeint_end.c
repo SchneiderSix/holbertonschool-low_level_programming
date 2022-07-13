01c42e7eeb7de58f6f973929e23ca457d8db0346
SchneiderSix <knuckles116635@gmail.com>
2022-07-13 05:09:14 -0700

    task three

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * add_nodeint - singly linked list
+ * @head: double pointer of listint_t
+ * @n: int var
+ * Return: count of nodes
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *nn, *ln = *head;
+
+	nn = malloc(sizeof(listint_t));
+
+	if (!nn)
+	{
+		return (NULL);
+	}
+
+	nn->next = NULL
+
+	if (*head == NULL)
+	{
+		*head = nn;
+		return (nn);
+	}
+
+	while (ln->next != NULL)
+	{
+		ln = ln->next;
+	}
+
+	ln->next = nn;
+
+	return (nn);
+}
