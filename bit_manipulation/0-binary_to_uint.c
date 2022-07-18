9bb9fddcdd69070982fee44ceed610317d12d556
SchneiderSix <knuckles116635@gmail.com>
2022-07-18 08:10:21 -0700

    task zero

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int i;
-    unsigned int dr = 0;
+	int i;
+	unsigned int dr = 0;
 
-    if (!b)
-    {
-        return (0);
-    }
+	if (!b)
+	{
+	return (0);
+	}
 
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-        {
-            return (0);
-        }
-        dr = dr * 2  + (b[i] - '0');
-    }
-    return (dr);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+	if (b[i] != '0' && b[i] != '1')
+	{
+	return (0);
+	}
+	dr = dr * 2  + (b[i] - '0');
+	}
+	return (dr);
 }
