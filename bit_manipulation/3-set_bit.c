05d4376fad225cf95a958d3599c86a1cb7abd2f8
SchneiderSix <knuckles116635@gmail.com>
2022-07-18 09:18:53 -0700

    task three

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * get_bit - explicit name
+ * @n: pointer to int
+ * @index: index
+ * Return: value or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+	*n |= 1UL << index;
+
+	return (1);
+}
