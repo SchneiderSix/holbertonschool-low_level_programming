e92f1b6d3ef6a7f3d52fe66d25adad9a5e840e50
SchneiderSix <knuckles116635@gmail.com>
2022-07-19 15:26:19 -0700

    task zero

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *
+ * read_textfile - reads text file and prints
+ * it to the POSIX standard output
+ * @letters - n of letters to be read
+ * Return: 0 if null or n of bytes that can read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t o, r, w;
+
+	if (!filename)
+	{
+		return (0);
+	}
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+	{
+		return (0);
+	}
+	o = open(filename, O_RDONLY);
+	r = read(o, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
+
+	if (r != w || o == -1 || r == -1 || w == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	free(buff);
+	close(o);
+	return (w);
+}
