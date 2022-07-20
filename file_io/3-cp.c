cb93e7659568dfe0352bced321b83aac869dc2fc
SchneiderSix <knuckles116635@gmail.com>
2022-07-19 20:05:01 -0700

    task three

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,7 +8,7 @@
 int main(int argc, char *argv[])
 {
 	char buff[1024];
-	int fdr, fdw, rbuff, cr, cw;
+	int fdr, fdw, rbuff = 1024, wbuff;
 
 	if (argc != 3)
 	{
@@ -23,32 +23,23 @@ int main(int argc, char *argv[])
 	}
 	fdw = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	rbuff = read(fdr, buff, 1024);
-	while (rbuff > 0)
+	while (rbuff == 1024)
 	{
-		if (fdw < 0 || write(fdw, buff, rbuff) != rbuff)
+		rbuff = read(fdr, buff, 1024);
+		if (rbuff == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fdr);
-			exit(99);
+			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 		}
-	}
-	if (rbuff == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	cr = close(fdr);
-	cw = close(fdw);
+		wbuff = write(fdw, buff, rbuff);
+		if (wbuff != rbuff)
+			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[1]), exit(99);
 
-	if (cr == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't close %d\n", fdr);
-		exit(100);
 	}
-	if (cw == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't close %d\n", fdw);
-		exit(100);
-	}
+	if (close(fdr) == -1)
+		dprintf(STDOUT_FILENO, "Error: Can't close %d\n", fdr), exit(100);
+			
+	if (close(fdw) == -1)
+		dprintf(STDOUT_FILENO, "Error: Can't close %d\n", fdr), exit(100);
+
 	return (0);
 }
