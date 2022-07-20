d20d23a90e4273fb076fd2744ce778ad9f41e51a
SchneiderSix <knuckles116635@gmail.com>
2022-07-19 19:37:00 -0700

    task three

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,22 +11,17 @@ int main(int argc, char *argv[])
 	int fdr, fdw, rbuff, cr, cw;
 
 	if (argc != 3)
-	{
 		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	}
 	fdr = open(argv[1], O_RDONLY);
-
 	if (fdr == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fdw = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	rbuff = read(fdr, buff, 1024);
-
-	while (rbuff != -1)
+	while (rbuff > 0)
 	{
 		if (fdw == -1 || write(fdw, buff, rbuff) != rbuff)
 		{
@@ -44,14 +39,10 @@ int main(int argc, char *argv[])
 	cw = close(fdw);
 
 	if (cr == -1)
-	{
 		dprintf(STDOUT_FILENO, "Error: Can't close %d\n", fdr);
 		exit(100);
-	}
-	if (cw == -1)
-	{
+	if (cw == -1
 		dprintf(STDOUT_FILENO, "Error: Can't close %d\n", fdw);
 		exit(100);
-	}
 	return (0);
 }
