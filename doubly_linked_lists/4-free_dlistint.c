83503673f27f7f8f317cc56f55ed5f776534e92f
SchneiderSix <knuckles116635@gmail.com>
2022-07-21 11:10:40 -0700

    task four

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlisint - free a list
+ * @head: pointer of dlistint_t
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
