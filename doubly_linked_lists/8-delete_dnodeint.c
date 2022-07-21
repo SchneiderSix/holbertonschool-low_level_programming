99edd8848c2a975f13546720baa9cf162333a64c
SchneiderSix <knuckles116635@gmail.com>
2022-07-21 13:02:31 -0700

    task eight

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - explicit name
+ * @head: double pointer of dlistint_t
+ * @index: index
+ * Return: 1 or -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *h, *dn;
+
+	if (!*head || head)
+	{
+		return (-1);
+	}
+	if (index == 0 && tmp->next != NULL)
+	{
+		dn = tmp;
+		tmp->next>prev = NULL;
+		tmp->next = *head;
+		free(dn);
+		return (1);
+	}
+	else if (index == 0 && tmp->next == NULL)
+	{
+		free(*head);
+		return (1);
+	}
+	while (tmp->next != NULL)
+	{
+		if (dlistint_len(tmp) == index)
+		{
+			dn = tmp;
+			tmp->next->prev = tmp->prev;
+			tmp->prev->next = tmp->next;
+		}	
+	}
+	if (index > dlistint_len(tmp))
+	{
+		return (-1);
+	}
+	free(dn);
+	return (1);
+}
