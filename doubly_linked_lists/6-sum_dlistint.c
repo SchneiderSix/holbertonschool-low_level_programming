570c41059fc92c1f401a7fe684e67a0d7cd4b236
SchneiderSix <knuckles116635@gmail.com>
2022-07-21 11:26:44 -0700

    task six

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dnodeint - sum all data
+ * @head: pointer of dlistint_t
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
