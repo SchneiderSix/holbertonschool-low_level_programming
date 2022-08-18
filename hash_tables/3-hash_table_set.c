fca03d171477a9828d36f59a0d95543842d67f1a
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-18 16:35:30 -0300

    Task two

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,1 +1,13 @@
-
+#include "hash_table_set"
+/**
+* key_index - map index to key
+* @key: key to be indexed
+* @size: size of ht
+* Return: n of op
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int n;
+	n = hash_djb2(key) % size;
+	return (n);
+}
