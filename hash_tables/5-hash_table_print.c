7b6edd3e98d7c93f116c63f4bb9b7b71912d28a3
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 15:00:15 -0300

    task five fix

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -8,15 +8,21 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
+	int coma = 0;
 
 	if (!ht)
 		return;
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		while (ht->array[i])
 		{
-			printf("'%s' : '%s'", ht->array[i]->key, ht->array[i]->value);
+			if (coma = 1)
+				printf(",");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			coma = 1;
 			ht->array[i] = ht->array[i]->next;
 		}
 	}
+	printf("}\n")
 }
