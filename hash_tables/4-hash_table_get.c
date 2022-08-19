6958297681a0b1cceb350a97ee211fa4507659f5
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 14:17:23 -0300

    Task 4 fix

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	if (!ht)
 	{
-		return (key);
+		return ((const cahr *)key);
 	}
 	if (ht->array[index]->key)
 	{
