6787442548942f85c088dcbb197397abd354c9b5
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 13:57:50 -0300

    Task four

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,1 +1,16 @@
+#include "hash_tables.h"
 
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index]->key)
+	{
+		return (ht->array[index]->value);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
