6b2be01aa1b02ac6d659e0743e4bff81c3c15b19
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 16:54:02 -0300

    update six

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -18,11 +18,11 @@ void hash_table_delete(hash_table_t *ht)
 
 		while (ht->array[i])
 		{
-			tmp = ht->array[i]->next;
-			free(ht->array[i]->key);
-			free(ht->array[i]->value);
-			free(ht->array[i]);
-			ht->array[i] = tmp;
+			tmp = ht->array[i];
+			ht->array[i] = ht->array[i]->next;
+			free(tmp->key);
+			free(tmp]->value);
+			free(tmp);
 		}
 	}
 	free(ht->array);
