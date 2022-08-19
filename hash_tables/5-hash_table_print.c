69e44b0965346fb614752cc2806eb8e2a611420a
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 14:46:12 -0300

    Task 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,1 +1,24 @@
+#include "hash_tables.h"
 
+/**
+* hash_table_print - print keys and values of ht
+* @ht: hash table
+* Return: nothing
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i, j = 0;
+
+	if (!ht)
+	{
+		return (NULL);
+	}
+	while (ht->array[j])
+	{
+		for (i = 0; ht->array[i] != '\0'; i++)
+		{
+			printf("%s:%s\n", ht->array[i]->key, ht->array[i]->value);
+		}
+		ht->array[j] = ht->array[j]->next;
+	}
+}
