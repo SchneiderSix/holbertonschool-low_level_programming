c3393526086cc113d50eb67e828d39a8b5d50380
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 15:02:24 -0300

    task five fix

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -18,7 +18,7 @@ void hash_table_print(const hash_table_t *ht)
 		while (ht->array[i])
 		{
 			if (coma == 1)
-				printf(",");
+				printf(", ");
 			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 			coma = 1;
 			ht->array[i] = ht->array[i]->next;
