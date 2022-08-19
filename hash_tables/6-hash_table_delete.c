cc871ed21a4e2fc42844f1b1f06a423c9bba8f50
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 15:08:05 -0300

    Task 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,1 +1,17 @@
+#include "hash_tables.h"
 
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int i;
+
+    for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			free(ht->array[i]->key);
+            free(ht->array[i]->value);
+			ht->array[i] = ht->array[i]->next;
+		}
+	}
+    free(ht);
+}
