11dbd088e73e84bc185dbb8da3d9aaa5b70bc9f1
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 15:12:41 -0300

    task 6 fix

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,17 +1,22 @@
 #include "hash_tables.h"
 
+/**
+* hash_table_delete - explicit name
+* Return: nothing
+*/
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
+	unsigned long int i;
 
-    for (i = 0; i < ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		while (ht->array[i])
 		{
 			free(ht->array[i]->key);
-            free(ht->array[i]->value);
+			free(ht->array[i]->value);
 			ht->array[i] = ht->array[i]->next;
 		}
 	}
-    free(ht);
+	free(ht->array), free(ht->size);
+	free(ht);
 }
