1b84ebc210d37280356daff599e284e9083a97b9
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 15:38:55 -0300

    task six fix

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -10,13 +10,14 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *tmp;
 
-	tmp = ht->array[i]->next;
+
 	if (!ht)
 		return;
 	for (i = 0; i < ht->size; i++)
 	{
 		while (ht->array[i])
 		{
+			tmp = ht->array[i]->next;
 			free(ht->array[i]->key);
 			free(ht->array[i]->value);
 			free(ht->array[i]);
