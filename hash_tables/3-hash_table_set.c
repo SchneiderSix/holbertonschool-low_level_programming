c19e9af16073a474ecafe6621893c7fe1978fb24
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 16:26:07 -0300

    update task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	nn = malloc(sizeof(hash_node_t));
 	if (!nn || !key || !value || !ht)
+	{
+		free(nn);
 		return (0);
+	}
 	index = key_index((const unsigned char *)key, ht->size);
 	nn->key = strdup((char *)key);
 	nn->value = strdup((char *)value);
