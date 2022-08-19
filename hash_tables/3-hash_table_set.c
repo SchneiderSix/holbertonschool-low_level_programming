526b81ca375cfb2cf5550f84b0998e6084b71bc4
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 11:47:48 -0300

    Task three

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -4,16 +4,34 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *tmp, *nn;
+	hash_node_t *nn = NULL;
 
-
-	if (!key || !value || !ht)
-		return (0);
 	nn = malloc(sizeof(hash_node_t));
-	if (!nd)
+	if (!nd || !key || !value || !ht)
 		return (0);
 	index = key_index((const char *)key, ht->size);
-	nn->key = key;
+	nn->key = strdup((char *)key);
 	nn->value = strdup((char *)value);
-	/*ht->array[index] = NULL;*/
+
+	/* replace node if null */
+	if (!ht->array[index])
+	{
+		ht->array[index] = nn;
+		nn->next = NULL;
+	}
+	else
+	{
+		/* if place is empty */
+		if (ht->array[index]->value == 0)
+		{
+			nn->next = ht->array[index];
+			ht->array[index] = nn;
+		}
+		else
+		{
+			free(ht->array[index]->value);
+			ht->array[index]->value = strdup((char *)value);
+			free(nn->key), free(nn);
+		}
+	}
 }
