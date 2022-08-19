a7615b03e1126e137a52f41874e3204e6610ac95
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-19 12:43:56 -0300

    Task 3 fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -30,7 +30,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		/* update value of key */
 		if (ht->array[index]->key == key)
 		{
-			free(nn);
 			free(ht->array[index]->value);
 			ht->array[index]->value = strdup((char *)value);
 		}
