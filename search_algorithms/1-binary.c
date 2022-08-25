1b5ce7f4da3b41fd3c7e0d30c4ebfb4146953884
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-25 18:14:10 -0300

    task one

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,6 +1,7 @@
 #include "search_algos.h"
 
-/** binary_search - search in array spliting in two
+/**
+* binary_search - search in array spliting in two
 * @array: array
 * @size: size of array
 * @value: value to be searched
