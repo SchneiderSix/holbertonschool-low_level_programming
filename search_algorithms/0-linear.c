ce2502ca9a23f6489a68100919cd9bd31323d5a7
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-25 13:53:32 -0300

    task zero

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,5 +1,12 @@
 #include "search_algos.h"
 
+/**
+* linear_search - search index of value in array
+* @array: array
+* @size: size of array
+* @value: value to be searched
+* Return: index or -1
+*/
 int linear_search(int *array, size_t size, int value)
 {
 	unsigned int i;
@@ -10,6 +17,7 @@ int linear_search(int *array, size_t size, int value)
 	}
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%d] = [%d]", i, value);
 		if (array[i] == value)
 			return (i);
 
