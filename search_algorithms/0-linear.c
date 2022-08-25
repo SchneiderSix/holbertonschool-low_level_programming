abbb3fac5caf32c582238f600179f1e7ed925eac
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-25 14:02:20 -0300

    task zero

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -12,7 +12,7 @@ int linear_search(int *array, size_t size, int value)
 	unsigned int i;
 
 	if (!array)
-		return -1
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, value);
